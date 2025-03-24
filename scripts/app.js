e3448bc9f1fc98b60ca5f878cf3a7cabc34590c3
JamesWrightSDGKU <James.Wright@sdgku.edu>
2025-03-23 17:49:18 -0700

    for Tim

--- scripts/app.js
+++ scripts/app.js
@@ -16,17 +16,52 @@ function saveTask(){
     console.log(taskToSave);
 
     // save to the server
+    $.ajax({
+        type: "POST",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks/", // server name
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
 
     // display the task
     displayTask(taskToSave);
+}
+
+function loadTask(){
+    
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net/api/tasks",
+        success: function(response){
+            console.log(response);
+            let data = JSON.parse(response);
+            console.log(data);
+            for(let i=0; i<data.length; i++){
+                let task = data[i];
+                if(task.name == "James")
+                {
+                    displayTask(task);
+                }
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
 }
 
 function displayTask(task){
     const event = new Date(task.startDate);
     const formattedDate = event.toDateString();
     let syntax = `
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
+        <div class="card" style="width: 18rem; border-color:${task.color}; border-width: 4px;">
+            <div class="card-body" style="background-color:mintcream">
                 <h3 class="card-title">${task.title}</h3>
                 <h4 class="card-subtitle mb-2 text-body-secondary">${task.status}</h4>
                 <h5 class="card-subtitle mb-2 text-body-secondary">${formattedDate}</h5>
@@ -35,17 +70,39 @@ function displayTask(task){
             </div>
         </div>
     `;
-    document.getElementById("list").style.backgroundColor = task.color;
+    //document.getElementById("list").style.backgroundColor = task.color;
     $(".pendingTask").append(syntax);
+}
+
+function deleteTasks(task){
+    $.ajax({
+        type: "DELETE",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function (response)
+    })
+}
+
+function testRequests(){
+    $.ajax({
+        type: "GET",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function (response){
+            console.log(response);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
 }
 
 function init(){
     console.log("I'm the init function");
 
     // load data
-
+    loadTask();
     // hook events
     $("#btnSave").click(saveTask);
+    $("#btnDelete").click(deleteTasks);
 }
 // new code change
 
